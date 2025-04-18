a53a8f95f11193abf8a10f77909c849fa0a14b50
jlnunezg10 <juniorng10@gmail.com>
2025-04-18 00:25:53 +0000

    First Commit/ Created User

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +14,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
